8ecffbe7b3b14670211141ba667402c95392e6a4
Ignacio Tampe <Igtampe@outlook.com>
2021-03-03 21:19:06 -0400

    Creation of basic battle junit

---
+++ backend/src/test/java/com/pistachio/main/test/BattleTest.java
@@ -0,0 +1,35 @@
+package com.pistachio.main.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import com.pistachio.main.Battle;
+
+/**
+ * Tests the battle class
+ * @author igtampe
+ *
+ */
+class BattleTest {
+
+	@Test
+	void EqualsTest() {
+		Battle B1 = new Battle("123", "Juan", new ArrayList<Object>(), "pepito", new ArrayList<Object>());
+		Battle B2 = new Battle("123", "Juan", new ArrayList<Object>(), "pepito", new ArrayList<Object>());
+		Battle B3 = new Battle("456", "Juan", new ArrayList<Object>(), "pepito", new ArrayList<Object>());
+		
+		assertTrue(B1.equals(B2));
+		assertFalse(B1.equals(B3));
+	}
+
+	@Test
+	void ActionsTest() {
+		Battle B1 = new Battle("123", "Juan", new ArrayList<Object>(), "pepito", new ArrayList<Object>());
+		B1.addAction("Help");
+		assertTrue(B1.getActionLog().contains("Help"));
+	}
+	
+}
