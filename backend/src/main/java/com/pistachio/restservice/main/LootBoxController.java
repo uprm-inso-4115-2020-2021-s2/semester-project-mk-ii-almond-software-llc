3c973f09c54ade50ee4392bbd4c518a8b6c6f732
Ignacio Tampe <Igtampe@outlook.com>
2021-04-14 16:54:04 -0400

    Lootbox Controller (adapted from Task Controller)

--- backend/src/main/java/com/pistachio/restservice/main/LootBoxController.java
+++ backend/src/main/java/com/pistachio/restservice/main/LootBoxController.java
@@ -1,5 +1,48 @@
 package com.pistachio.restservice.main;
 
-public class LootBoxController {
-    
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "https://almond-pistachio-front-end.herokuapp.com")
+public class LootBoxController
+{
+    @Autowired
+    private LootBoxRepository LootBoxRepo;
+
+    @PostMapping("/lootbox/add")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public LootBox add(@RequestBody LootBox lb) {return LootBoxRepo.save(lb);}
+
+    @GetMapping("/lootboxes")
+    public List<LootBox> getAll() {return LootBoxRepo.findAll();}
+
+    @GetMapping(value = "/lootbox/{id}")
+    public LootBox getOne(@PathVariable String id) {
+        return LootBoxRepo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException());
+    }
+
+    @PutMapping(value = "/lootbox/{id}")
+    public LootBox update(@PathVariable String id, @RequestBody LootBox updatedLB) {
+        LootBox lb = LootBoxRepo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException());
+//        task.setName(updatedTask.getName());
+//        task.setDescription(updatedTask.getDescription());
+//        task.setReward(updatedTask.getReward());
+//        task.setCompletionCriteria(updatedTask.getCompletionCriteria());
+        return LootBoxRepo.save(lb);
+    }
+
+    @DeleteMapping(value = "/lootbox/{id}")
+    @ResponseStatus(code = HttpStatus.ACCEPTED)
+    public void delete(@PathVariable String id) {
+        LootBox LB = LootBoxRepo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException());
+        LootBoxRepo.delete(LB);
+    }
 }
