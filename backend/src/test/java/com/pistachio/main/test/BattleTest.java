71b420f271a44fdf96f38b31816a3fc36e065a7e
Ignacio Tampe <Igtampe@outlook.com>
2021-04-14 15:57:35 -0400

    Fix BattleTest Constructor Errors

--- backend/src/test/java/com/pistachio/main/test/BattleTest.java
+++ backend/src/test/java/com/pistachio/main/test/BattleTest.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 
 import com.pistachio.restservice.main.Battle;
+import com.pistachio.restservice.main.Monster;
 
 /**
  * Tests the battle class
@@ -17,9 +18,9 @@ class BattleTest {
 
 	@Test
 	void EqualsTest() {
-		Battle B1 = new Battle("123", "Juan", new ArrayList<Object>(), "pepito", new ArrayList<Object>());
-		Battle B2 = new Battle("123", "Juan", new ArrayList<Object>(), "pepito", new ArrayList<Object>());
-		Battle B3 = new Battle("456", "Juan", new ArrayList<Object>(), "pepito", new ArrayList<Object>());
+		Battle B1 = new Battle("123", "Juan", new ArrayList<Monster>(), "pepito", new ArrayList<Monster>());
+		Battle B2 = new Battle("123", "Juan", new ArrayList<Monster>(), "pepito", new ArrayList<Monster>());
+		Battle B3 = new Battle("456", "Juan", new ArrayList<Monster>(), "pepito", new ArrayList<Monster>());
 		
 		assertTrue(B1.equals(B2));
 		assertFalse(B1.equals(B3));
@@ -27,9 +28,9 @@ class BattleTest {
 
 	@Test
 	void ActionsTest() {
-		Battle B1 = new Battle("123", "Juan", new ArrayList<Object>(), "pepito", new ArrayList<Object>());
-		// B1.addAction("Help");
-		// assertTrue(B1.getActionLog().contains("Help"));
+		//Battle B1 = new Battle("123", "Juan", new ArrayList<Monster>(), "pepito", new ArrayList<Monster>());
+		//B1.addAction("Help");
+		//assertTrue(B1.getActionLog().contains("Help"));
 	}
 	
 }
